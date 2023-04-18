db11104d509d380ac41aaf58bdcfa51cac3f2fd4
newtreenoh <newtreenoh@gmail.com>
2023-04-18 11:19:57 +0100

    Initial script commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// DOM elements
+const TuDuForm = document.querySelector('#TuDuForm');
+const TuDuList = document.querySelector('.TuDus')
+const remainingTasks = document.querySelector('#remainingTasks')
+const completedTasks = document.querySelector('#completedTasks')
+const totalTasks = document.querySelector('#totalTasks')
+const addTask = document.querySelector('#addTask')
