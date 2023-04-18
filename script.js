d74d455eb42caf4c18276776d10f2898f2605f4b
newtreenoh <newtreenoh@gmail.com>
2023-04-18 11:42:51 +0100

    Added basic functions to the script

--- script.js
+++ script.js
@@ -5,3 +5,41 @@ const remainingTasks = document.querySelector('#remainingTasks')
 const completedTasks = document.querySelector('#completedTasks')
 const totalTasks = document.querySelector('#totalTasks')
 const addTask = document.querySelector('#addTask')
+
+// check for 'tasks' in local storage using JSON object and turn them into array or get empty array
+let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+TuDuForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    const inputValue = addTask.value
+
+    if(inputValue == ''){
+        return
+    }
+
+    const task = {
+        id: new Date().getTime(),
+        name: inputValue,
+        isComplete: false,
+    }
+
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+
+    createTasks(task)
+
+    TuDuForm.reset()
+    addTask.focus()
+})
+
+
+function createTasks(tasks){
+    const task = document.createElement('li')
+
+    task.setAttribute('id', task.id)
+
+    if(task.isComplete){
+        task.classList.add('complete')
+    }
+}
