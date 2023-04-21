03d8ed6e3f73f7f5071ae6f8cf5454ed61cca665
newtreenoh <newtreenoh@gmail.com>
2023-04-21 14:00:00 +0100

    Changed index.html

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // DOM elements
-const TuDuForm = document.querySelector('#TuDuForm');
+const TuDuForm = document.getElementById('TuDuForm');
 const TuDuList = document.querySelector('.TuDus')
-const remainingTasks = document.querySelector('#remainingTasks')
-const completedTasks = document.querySelector('#completedTasks')
-const totalTasks = document.querySelector('#totalTasks')
-const addTask = document.querySelector('#addTask')
+const remainingTasks = document.getElementById('remainingTasks')
+const completedTasks = document.getElementById('completedTasks')
+const totalTasks = document.getElementById('totalTasks')
+const addTask = document.getElementById('addTask')
+const btnSubmit = document.getElementById('btnSubmit');
+
+let tudus = [];
+
+btnSubmit.addEventListener('click', createNewTuDu);
+
+function createNewTuDu(){
+    // Task structure
+    const item = {
+        id: new Date().getTime(),
+        text: "",
+        complete:  false
+    }
+
+    // to add item at the top
+    tudus.unshift(item);
+}
+
