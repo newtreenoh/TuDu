4f318bcd11b5eb971a3e53c5dc0d9d62588ec9ae
newtreenoh <newtreenoh@gmail.com>
2023-04-22 21:47:32 +0100

    Script changed to include function

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 // DOM elements
-const TuDuForm = document.getElementById('TuDuForm');
-const TuDuList = document.querySelector('.TuDus')
-const remainingTasks = document.getElementById('remainingTasks')
-const completedTasks = document.getElementById('completedTasks')
-const totalTasks = document.getElementById('totalTasks')
-const addTask = document.getElementById('addTask')
-const btnSubmit = document.getElementById('btnSubmit');
+const TuDuForm       = document.getElementById('TuDuForm');
+const TuDuList       = document.querySelector('.TuDus');
 
-let tudus = [];
+const remainingTasks = document.getElementById('remainingTasks');
+const completedTasks = document.getElementById('completedTasks');
+const totalTasks     = document.getElementById('totalTasks');
 
-btnSubmit.addEventListener('click', createNewTuDu);
+const addTask        = document.getElementById('addTask');
+const btnSubmit      = document.getElementById('btnSubmit');
 
-function createNewTuDu(){
-    // Task structure
-    const item = {
-        id: new Date().getTime(),
-        text: "",
-        complete:  false
+// Event Listeners
+btnSubmit.addEventListener('click', createTuDu);
+
+// Functions
+function createTuDu(){
+    try {
+        if(addTask.value != ''){
+            const div = document.createElement('div');
+    
+            div.className = 'tuDuTask';
+    
+            div.innerHTML = `
+            <li>
+                <div>
+                    <input type="checkbox" name="tasks" id="1">
+                    <span>addTask.value</span>
+                </div>
+                <button title="removeTask" class="removeTask">
+                    X
+                </button>
+            </li>
+            `;
+    
+            TuDuList.appendChild(div);
+            addTask.value = '';         // reset input field to empty for next use
+        }else {
+            alert('Please add a TuDu to add.')
+        }
+    } catch (error) {
+        console.log(error);
     }
-
-    // to add item at the top
-    tudus.unshift(item);
 }
-
