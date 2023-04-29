07aa49409104b39dd4866efa21c2165a99cc9ce3
newtreenoh <newtreenoh@gmail.com>
2023-04-29 11:11:00 +0100

    Changed script

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 // DOM elements
 const btnAddTask = document.getElementById('btnAddTask');
+const tuDuContent = document.getElementById('tuDuContent');
 
-const tuDuContent = document.getElementById('tuDuContent')
+const addTuDuText = document.getElementById('addTuDuText');
+const btnDelete = document.getElementById('btnDelete');
 
 // Event Listeners
 btnAddTask.addEventListener('click', addTuDu);
+tuDuContent.addEventListener('click', removeTuDu);
 
 // Functions
 function addTuDu(){
     try {
         if(addTuDuText.value != ''){
             const div = document.createElement('div');
-    
             div.className = 'tuDuTask';
-    
             div.innerHTML = `
-            <div class="tuDuTask" id="tuDuTask">
+            <ul class="tuDuTask">
+                <li>
                 <input type="checkbox" name="" id="tuDuCheckbox">
-                <p class="taskText" id="taskText">` + addTuDuText.value + `</p>
-                <button class="btnDelete" id="btnDelete">X</button>
-            </div>
+                <p class="taskText">` + addTuDuText.value + `</p>
+                <button class="btnDelete">X</button>
+                </li>
+            </ul>
             `;
-    
             tuDuContent.appendChild(div);
             addTuDuText.value = '';         // reset input field to empty for next use
-        }else {
+        } else {
             alert('Please add a TuDu to add.')
         }
     } catch (error) {
         console.log(error);
     }
 }
+
+function removeTuDu() {
+    
+}
+  
+  
